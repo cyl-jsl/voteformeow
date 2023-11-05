049376f68a551a33c7111582bc546e43c42c8feb
CYL <cyl.jim@gmail.com>
2023-11-06 02:27:15 +0800

    init commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Layout from "./Layout";
+import Homepage from "./pages/Homepage";
+import NotFound from "./pages/NotFound";
+import "./styles/style.css";
 
 function App() {
   return (
-    <h1>XD</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Homepage />}/>
+          <Route path="*" element={<NotFound/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/Layout.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+import Footer from "./components/Footer";
+
+const Layout = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li> LOGO</li>
+        </ul>
+        <ul>
+          <li>候選人主張</li>
+          <li>最新活動</li>
+          <li>政策議題</li>
+          <li>小額捐款</li>
+          <li>民眾服務信箱</li>
+        </ul>
+        <ul>
+          <li>fb</li>
+          <li>in</li>
+          <li>yt</li>
+        </ul>
+      </nav>
+      <Outlet/>
+      <Footer/>
+    </div>
+  );
+};
+
+export default Layout;
